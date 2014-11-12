64bf272a498474607db5845efb4445f417e92a93
Clive C. Shumba <clive@cornelo.us>
2014-11-12 12:50:54 +0200

    Update

--- application/classes/controller/application.php
+++ application/classes/controller/application.php
@@ -1,9 +1,14 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 abstract class Controller_Application extends Controller_Template {
+    public $assert_auth = FALSE;
+    public $assert_auth_actions = FALSE;
+
+
     public function before()
     {
         parent::before();
+        $this->_user_auth();
 
         //Global variables
         View::set_global('site_name', 'Mukuru Assignment');
@@ -11,5 +16,18 @@ abstract class Controller_Application extends Controller_Template {
         $this->template->view = '';
         $this->template->styles = array('reset', 'mukuru');
         $this->template->scripts = array();
+    }
+
+    protected function _user_auth()
+    {
+        $session = Session::instance();
+        $username = $session->get('username');
+        
+
+        if (!$username){
+
+            Request::instance()
+                ->redirect('login');
+        }
     }
 }
