7c9bb8bc52ffb86385b6fd639ac4ee51e72b793d
Clive C. Shumba <clive@cornelo.us>
2014-11-12 03:43:24 +0200

    4 days of Kohana

---
+++ application/classes/controller/application.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Controller_Application extends Controller_Template {
+    public function before()
+    {
+        parent::before();
+
+        //Global variables
+        View::set_global('site_name', 'Mukuru Assignment');
+
+        $this->template->view = '';
+        $this->template->styles = array('reset', 'mukuru');
+        $this->template->scripts = array();
+    }
+}
---
+++ application/classes/controller/welcome.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Welcome extends Controller_Application {
+
+	public function action_index()
+	{
+        $view = View::factory('welcome')
+            ->bind('users', $users)
+            ->bind('pager_links', $pager_links);
+
+        $user = new Model_User;
+        $user_count = $user->count_all();
+
+        $pagination = Pagination::factory(array(
+            'total_items'
+            => $user_count,
+            'items_per_page' => 3,
+        ));
+        $pager_links = $pagination->render();
+
+
+        $users = $user->get_all($pagination->items_per_page,
+            $pagination->offset);
+
+        $this->template->view = $view;
+	}
+
+
+
+} // End Welcome
